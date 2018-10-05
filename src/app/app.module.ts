830dc42ae7325c27e06d4b0d04f2837841bbebdb
Lucas H. Abreu <lucas.henrique@DL-LUCASABREU.vms.com.br>
2018-10-05 17:08:28 -0300

    adicionado flexLayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, MatCardModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ApiService } from './services/api.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ScrollDirective } from './directives/scroll.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DetailsComponent,
+    ScrollDirective
   ],
   imports: [
     HttpClientModule,
+    FlexLayoutModule,
 
     BrowserModule,
     BrowserAnimationsModule,
